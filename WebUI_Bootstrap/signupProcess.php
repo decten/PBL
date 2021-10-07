34eb1767c2c1b1ae2d4830800882ca8a20df2b13
성채민 <cmalice@naver.com>
2021-10-08 01:15:58 +0900

    signupProcess.php

---
+++ WebUI_Bootstrap/signupProcess.php
@@ -0,0 +1,25 @@
+<?php
+
+    $conn = mysqli_connect("15.164.219.223", "pbl", "pbl", "pbl") or die ("connect fail");
+
+    $id = $_POST['signup-ID'];
+    $password = $_POST['signup-password'];
+    $name = $_POST['signup-name'];
+    $birth = $_POST['signup-birth'];
+    $gender = $_POST['signup-gender'];
+    $email = $_POST['signup-email'];
+    $number = $_POST['signup-number'];
+    $company = $_POST['signup-company'];
+
+
+    $sql = "insert into signup (id, password, name, birth, gender, email, phone, company) values ('$id', '$password', '$name', '$birth', '$gender', '$email', '$number', '$company')";
+
+    $result = $conn->query($sql);
+
+    if ($result === false) {
+        echo mysqli_error($conn);   
+    } else {
+        echo "<script>alert('회원가입 완료');</script>";
+        echo "<script>window.location.replace('login.html');</script>";
+    }
+?>
